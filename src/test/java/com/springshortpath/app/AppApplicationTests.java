c81096c439e9672a9a25d1ee1b66167b3aa97cb3
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-06-15 20:57:21 +0300

    Task 41 : Define listCities, cityById, cityByName, updateCity test method in AppApplicationTests

--- src/test/java/com/springshortpath/app/AppApplicationTests.java
+++ src/test/java/com/springshortpath/app/AppApplicationTests.java
@@ -1,18 +1,27 @@
 package com.springshortpath.app;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.Neo4jContainer;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.UUID;
 
 @SpringBootTest
 class AppApplicationTests {
 
     static Neo4jContainer<?> container = new Neo4jContainer<>("neo4j:4.3-community");
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
@@ -20,6 +29,8 @@ class AppApplicationTests {
         registry.add("spring.neo4j.authentication.password", () -> "123456");
         registry.add("spring.neo4j.uri", () -> container.getBoltUrl());
     }
+
+    private final UUID cityId1 = UUID.randomUUID();
 
     @BeforeAll
     static void startContainer() {
@@ -33,18 +44,54 @@ class AppApplicationTests {
     }
 
     @Test
-    void listCities() {
-
+    void listCities() throws Exception {
+        try {
+            mockMvc.perform(get("/api/v1/city/cities").accept("application/json"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$[0]['name']").value("Istanbul"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void cityById(){
-
+    void cityById() throws Exception {
+        try {
+            mockMvc.perform(get("/api/v1/city/id/" + cityId1).accept("application/json"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value("Istanbul"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void cityByName(){
+    void cityByName() throws Exception {
+        try {
+            mockMvc.perform(get("/api/v1/city/name/Istanbul").accept("application/json"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value("Istanbul"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    void updateCity(){
+        try {
+            mockMvc.perform(put("/api/v1/city/id/" + cityId1).accept("application/json"))
+                    .contentType("application/json")
+                    .content("{\"name\" : \"Ankara\"}")
+                    .accept("application/json"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value("Ankara"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
