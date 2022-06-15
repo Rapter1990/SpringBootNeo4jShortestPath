5257c7e27648a190db2d086db4cf0b3bf74310d4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-06-15 13:19:41 +0300

    Task 40 : Create a test template containing all methods in AppApplicationTests

--- src/test/java/com/springshortpath/app/AppApplicationTests.java
+++ src/test/java/com/springshortpath/app/AppApplicationTests.java
@@ -1,13 +1,79 @@
 package com.springshortpath.app;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.Neo4jContainer;
 
 @SpringBootTest
 class AppApplicationTests {
 
-    @Test
-    void contextLoads() {
+    static Neo4jContainer<?> container = new Neo4jContainer<>("neo4j:4.3-community");
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.neo4j.authentication.username", () -> "neo4j");
+        registry.add("spring.neo4j.authentication.password", () -> "123456");
+        registry.add("spring.neo4j.uri", () -> container.getBoltUrl());
     }
 
+    @BeforeAll
+    static void startContainer() {
+        container.start();
+    }
+
+
+    @BeforeEach
+    void initializeData() {
+
+    }
+
+    @Test
+    void listCities() {
+
+    }
+
+    @Test
+    void cityById(){
+
+    }
+
+    @Test
+    void cityByName(){
+
+    }
+
+    @Test
+    void addRoute(){
+
+    }
+
+    @Test
+    void listRoutesByCity(){
+
+    }
+
+    @Test
+    void deleteRoute(){
+
+    }
+
+    @Test
+    void shortestPath(){
+
+    }
+
+    @Test
+    void shortestPathInTime(){
+
+    }
+
+    @AfterAll
+    static void tearDownContainer() {
+        container.stop();
+    }
 }
