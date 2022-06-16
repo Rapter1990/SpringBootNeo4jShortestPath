4ad78c9345e1d3fadbd6d6c89ddd336a670acd83
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-06-16 23:26:06 +0300

    Task 45 : Revise some test methods and complote rest methods in AppApplicationTests

--- src/test/java/com/springshortpath/app/AppApplicationTests.java
+++ src/test/java/com/springshortpath/app/AppApplicationTests.java
@@ -1,6 +1,5 @@
 package com.springshortpath.app;
 
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,33 +7,45 @@ import org.junit.jupiter.api.Test;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.Neo4jContainer;
+import org.neo4j.driver.Record;
 import org.testcontainers.containers.Neo4jLabsPlugin;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureWebTestClient
 class AppApplicationTests {
 
-    static Neo4jContainer<?> container = new Neo4jContainer<>("neo4j:4.3-community");
+    static Neo4jContainer<?> container = new Neo4jContainer<>("neo4j:4.3-community")
+            .withLabsPlugins(Neo4jLabsPlugin.APOC);
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private Driver driver;
+
+    @Autowired
+    private WebTestClient webTestClient;
 
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
@@ -71,7 +82,8 @@ class AppApplicationTests {
             mockMvc.perform(get("/api/v1/city/cities").accept("application/json"))
                     .andDo(print())
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$[0]['name']").value("Istanbul"));
+                    .andExpect(jsonPath("$[0]['name']").value("Istanbul"))
+                    .andExpect(jsonPath("$[1]['name']").value("Ankara"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -84,6 +96,13 @@ class AppApplicationTests {
                     .andDo(print())
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.name").value("Istanbul"));
+
+            // ensure optional match works and returns a city without a route relationship
+            mockMvc.perform(get("/api/v1/city/id/" + cityId2).accept("application/json"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value("Ankara"));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -117,28 +136,68 @@ class AppApplicationTests {
     }
 
     @Test
-    void addRoute(){
+    void addRoute() throws Exception {
+        mockMvc.perform(post("/api/v1/route/" + cityId1 + "/create-route")
+                .contentType("application/json")
+                .content("{\"from\" : \"Istanbul\", \"destination\" : \"Ankara\", \"departureTime\" : \"9:00\", \"arriveTime\" : \"11:00\"}")
+                .accept("application/json"))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.from").value("Istanbul"))
+                .andExpect(jsonPath("$.destination").value("Ankara"));
 
+        try (Session session = driver.session()) {
+            List<Record> records = session.run("MATCH (c:City) return c.name as cityName").list();
+            assertThat(records).hasSize(2);
+            assertThat(records).map(record -> record.get("cityName").asString())
+                    .containsExactlyInAnyOrder("Ankara", "Istanbul");
+        }
     }
 
     @Test
-    void listRoutesByCity(){
-
+    void listRoutesByCity() throws Exception {
+        mockMvc.perform(get("/api/v1/route/" +  cityId1 + "/routes").accept("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]['from']").value("Istanbul"))
+                .andExpect(jsonPath("$[0]['destination']").value("Ankara"));
     }
 
     @Test
-    void deleteRoute(){
+    void deleteRoute() throws Exception {
+        mockMvc.perform(delete("/api/v1/route/" + cityId1 + "/delete-route/" + routeId)
+                .accept("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk());
 
+        try (Session session = driver.session()) {
+            long routeCount = session.run("MATCH (r:ROUTE) return count(r) as routeCount").single().get("routeCount").asLong();
+            assertThat(routeCount).isEqualTo(0L);
+        }
     }
 
     @Test
     void shortestPath(){
-
+        webTestClient.method(HttpMethod.GET).uri("/api/v1/shortestpath/shortest-path")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue("{\"from\":\"Istanbul\", \"destination\":\"Ankara\"}")
+                .header("content-type", "application/json")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .json("{\"departureCity\":\"Istanbul\",\"arrivalCity\":\"Ankara\",\"totalConnections\":1}");
     }
 
     @Test
     void shortestPathInTime(){
-
+        webTestClient.method(HttpMethod.GET).uri("/api/v1/shortestpath/shortest-path-in-time")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue("{\"from\":\"Istanbul\", \"destination\":\"Ankara\"}")
+                .header("content-type", "application/json")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .json("{\"departureCity\":\"Istanbul\",\"arrivalCity\":\"Ankara\",\"totalInTime\":2}");
     }
 
     @AfterAll
