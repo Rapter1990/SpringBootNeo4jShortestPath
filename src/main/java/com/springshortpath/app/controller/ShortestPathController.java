7677c6deae02205c6736c82b9110ce07a5ae8c32
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-06-20 00:03:36 +0300

    Task 47 : Add some logs in getShortestPath and getShortestPathInTime methods of ShortestPathController

--- src/main/java/com/springshortpath/app/controller/ShortestPathController.java
+++ src/main/java/com/springshortpath/app/controller/ShortestPathController.java
@@ -4,7 +4,8 @@ import com.springshortpath.app.payload.request.PathRequest;
 import com.springshortpath.app.payload.response.PathShortestConnectionResponse;
 import com.springshortpath.app.payload.response.PathShortestTimeResponse;
 import com.springshortpath.app.service.ShortestPathService;
-import org.springframework.core.io.PathResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -12,6 +13,8 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping(path = "/api/v1/shortestpath")
 public class ShortestPathController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     private final ShortestPathService shortestPathService;
 
@@ -29,6 +32,10 @@ public class ShortestPathController {
     @GetMapping("/shortest-path")
     public Mono<PathShortestConnectionResponse> getShortestPath(@RequestBody PathRequest pathRequest) {
 
+        LOGGER.info("ShortestPathController | getShortestPath is started");
+        LOGGER.info("ShortestPathController | getShortestPath | pathRequest from : " + pathRequest.getFrom());
+        LOGGER.info("ShortestPathController | getShortestPath | pathRequest destination : " + pathRequest.getDestination());
+
         return shortestPathService.getShortestPath(pathRequest.getFrom(), pathRequest.getDestination())
                 .map(PathShortestConnectionResponse::new)
                 .switchIfEmpty(Mono.error(new IllegalArgumentException("Error")));
@@ -43,6 +50,11 @@ public class ShortestPathController {
      */
     @GetMapping("/shortest-path-in-time")
     public Mono<PathShortestTimeResponse>  getShortestPathInTime(@RequestBody PathRequest pathRequest) {
+
+        LOGGER.info("ShortestPathController | getShortestPathInTime is started");
+        LOGGER.info("ShortestPathController | getShortestPathInTime | pathRequest from : " + pathRequest.getFrom());
+        LOGGER.info("ShortestPathController | getShortestPathInTime | pathRequest destination : " + pathRequest.getDestination());
+
 
         return shortestPathService.getShortestPathInTime(pathRequest.getFrom(), pathRequest.getDestination())
                 .map(PathShortestTimeResponse::new)
