381c4df54e980948ff1b17f370acd160f2cd14b4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-06-24 16:12:17 +0300

    Task 64 : Fix some codes in initializeData method of AppApplicationTests

--- src/test/java/com/springshortpath/app/AppApplicationTests.java
+++ src/test/java/com/springshortpath/app/AppApplicationTests.java
@@ -72,23 +72,20 @@ public class AppApplicationTests {
     void initializeData() {
         try (Session session = driver.session()) {
             session.run("MATCH (n) detach delete n").consume();
-            session.run("MERGE (:City{id:$cityId1, name:'Istanbul'})" +
-                            "-[:ROUTES]->(:Route{id:$routeId1,from:'Istanbul', destination:'Ankara', duration: 2}) " +
-                            "-[:ROUTES]->(:City{id:$cityId2, name:'Ankara'})",
-                    Map.of("cityId1", cityId1.toString(), "cityId2", cityId2.toString(), "routeId1", routeId1.toString()))
-                    .consume();
-
-            session.run("MERGE (:City{id:$cityId1, name:'Istanbul'})" +
-                            "-[:ROUTES]->(:Route{id:$routeId2,from:'Istanbul', destination:'Antalya', duration: 3}) " +
-                            "-[:ROUTES]->(:City{id:$cityId3, name:'Antalya'})",
-                    Map.of("cityId1", cityId1.toString(), "cityId3", cityId3.toString(), "routeId2", routeId2.toString()))
-                    .consume();
-
-            session.run("MERGE (:City{id:$cityId2, name:'Ankara'})" +
-                            "-[:ROUTES]->(:Route{id:$routeId3,from:'Ankara', destination:'Antalya', duration: 2}) " +
-                            "-[:ROUTES]->(:City{id:$cityId3, name:'Antalya'})",
-                    Map.of("cityId2", cityId2.toString(), "cityId3", cityId3.toString(), "routeId3", routeId3.toString()))
-                    .consume();
+            session.run("MERGE (:City1{id:$cityId1, name:'Istanbul'})" +
+                            "MERGE (:City2{id:$cityId2, name:'Ankara'})" +
+                            "MERGE (:City3{id:$cityId3, name:'Antalya'})" +
+                            "MERGE (City1)-[:ROUTES]->(:Route{id:$routeId1,from:'Istanbul', destination:'Ankara', duration: 2})-[:ROUTES]->(City2)" +
+                            "MERGE (City1)-[:ROUTES]->(:Route{id:$routeId2,from:'Istanbul', destination:'Antalya', duration: 3})-[:ROUTES]->(City3)" +
+                            "MERGE (City2)-[:ROUTES]->(:Route{id:$routeId3,from:'Ankara', destination:'Antalya', duration: 2})-[:ROUTES]->(City3)",
+                    Map.of( "cityId1", cityId1.toString(),
+                            "cityId2", cityId2.toString(),
+                            "cityId3", cityId3.toString(),
+                            "routeId1", routeId1.toString(),
+                            "routeId2", routeId2.toString(),
+                            "routeId3", routeId2.toString()
+                    )
+            );
         }
     }
 
